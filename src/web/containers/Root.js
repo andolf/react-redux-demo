d72a32808ff3b43d35604fe218a6445103c7769c
Christian Andolf <christian@weahead.se>
2017-05-10 22:54:24 +0200

    Added devtools.

--- src/web/containers/Root.js
+++ src/web/containers/Root.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Provider} from 'react-redux';
 import Routes from '../routes';
+import DevTools from '../../core/components/DevTools';
 
 class Root extends React.Component {
     static propTypes = {
@@ -17,7 +18,10 @@ class Root extends React.Component {
 
         return (
             <Provider store={store}>
-                <Routes />
+                <div>
+                    <Routes />
+                    <DevTools />
+                </div>
             </Provider>
         );
     }
