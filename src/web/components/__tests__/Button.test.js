0efa4ed2ea3b315da30dc856d66cf2bc5bcbfa64
Christian Andolf <christian@weahead.se>
2020-01-01 21:16:25 +0100

    Added react testing library

--- src/web/components/__tests__/Button.test.js
+++ src/web/components/__tests__/Button.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import Button from '../Button';
 
+test('Button renders with children', () => {
+  const { container } = render(<Button>Text</Button>);
+  expect(container.firstChild).toHaveTextContent('Text');
+});
+
 test('Button renders with default type', () => {
-    const component = renderer.create(
-        <Button>Text</Button>
-    );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+  const { container } = render(<Button>Text</Button>);
+  expect(container.firstChild).toHaveAttribute('type', 'button');
+});
+
+test('Button renders with added class', () => {
+  const { container } = render(<Button className="foo">Text</Button>);
+  expect(container.firstChild).toHaveAttribute('class', 'foo');
 });
